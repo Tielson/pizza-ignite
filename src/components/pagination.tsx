5402b06dc22a5439f2e6e393cc3609848c8307c6
filipetielson <filipe.tielson@plugoo.com>
2024-06-05 08:02:46 -0300

    fix: facture

---
+++ src/components/pagination.tsx
@@ -0,0 +1,22 @@
+export interface PaginationProps {
+  pageIndex: number
+  totalCount: number
+  perPage: number
+}
+
+export function Pagination({
+  pageIndex,
+  perPage,
+  totalCount,
+}: PaginationProps) {
+  const pages = Math.ceil(perPage / totalCount) || 1
+  return (
+    <div className="flex items-center justify-between">
+      <span className="text-sm text-muted-foreground">
+        Total de {totalCount} item(s)
+      </span>
+
+      <div className="flex items-center gap-6 lg:gap-8"></div>
+    </div>
+  )
+}
